ede5b03bea719c70fdf962c74941a6602b91914c
Devesh Mishra <devesh.mishra@intrcity.com>
2023-11-29 00:26:25 +0530

    array datatype introduction with PersonList

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import './App.css';
+import "./App.css";
 
-import { Greet } from './components/Greet';
-import { Person } from './components/Person';
+import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
   const personName = {
-    first:`Devesh`,
-    last:`Mishra`
-  } 
+    first: `Devesh`,
+    last: `Mishra`,
+  };
+  const nameList = [
+    {
+      first: `Devesh`,
+      last: `Mishra`,
+    },
+    {
+      first: `Sarvesh`,
+      last: `Mishra`,
+    },
+    {
+      first: `Pravesh`,
+      last: `Mishra`,
+    },
+  ];
+
   return (
     <div className="App">
-      <Greet name='Devesh' messageCount={10} isLoggedIn={false}/>
-      <Person name={personName}/>
+      <Greet name="Devesh" messageCount={10} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList}/>
     </div>
   );
 }
