e0a3c9ca313a984f07fcb8c9ad30a2fbe50742e0
Devesh Mishra <devesh.mishra@intrcity.com>
2023-11-29 00:17:58 +0530

    typecasting of an object example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css';
 
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
 
 function App() {
+  const personName = {
+    first:`Devesh`,
+    last:`Mishra`
+  } 
   return (
     <div className="App">
       <Greet name='Devesh' messageCount={10} isLoggedIn={false}/>
+      <Person name={personName}/>
     </div>
   );
 }
