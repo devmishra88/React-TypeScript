d69faf27a44c12aacc77497a0ded0e26d60dae42
Devesh Mishra <devesh.mishra@intrcity.com>
2023-11-30 23:43:58 +0530

    children props typecasting applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
 import { Status } from "./components/Status";
@@ -31,6 +32,7 @@ function App() {
       <Person name={personName} />
       <PersonList names={nameList}/> */}
       <Status status="error"/>
+      <Heading>Placeholder Text</Heading>
     </div>
   );
 }
