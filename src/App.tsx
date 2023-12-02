550f295e5bd049ed1d700e7254f996707f79b649
Devesh Mishra <devesh.mishra@intrcity.com>
2023-12-02 23:17:55 +0530

    style type props intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Button } from "./components/Button";
+import { Container } from "./components/Container";
 
 import { Greet } from "./components/Greet";
 import { Heading } from "./components/Heading";
@@ -43,6 +44,10 @@ function App() {
         console.log(`I am Devesh`,event, id)
       }} />
       <Input value="" handleChange={event=>console.log(event)}/>
+      <Container styles={{
+        border:`1px solid red`,
+        padding:`1rem`
+      }}/>
     </div>
   );
 }
